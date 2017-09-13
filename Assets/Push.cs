940a0c8699adc7329f8f48d2135fe44cb70b5b37
Federico Ramundo <federico.ramundo@wolox.com.ar>
2017-09-13 18:55:57 -0300

    demo scenes & gvr

---
+++ Assets/Push.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour {
+
+	public float force = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ApplyForce() {
+		GetComponent<Rigidbody> ().AddForce (transform.forward * force);
+	}
+}
